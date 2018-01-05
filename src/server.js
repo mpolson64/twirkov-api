1c5967ca7cc31750ca25dbc23e68b2c0b81df048
Miles Olson <mpolson64@gmail.com>
2018-01-04 17:37:02 -0800

    Finished cleaning

--- src/server.js
+++ src/server.js
@@ -14,6 +14,4 @@ app.use(cors());
 app.use('/api/chain', chainRoute);
 app.use('/api/seed', seedRoute);
 
-app.listen(port, () => {
-  console.log(`twirkov-api listening on port ${port}`);
-});
+app.listen(port);
--- src/util/generatorLoop.js
+++ src/util/generatorLoop.js
@@ -1,14 +1,14 @@
 const feedforward = require('./feedforward');
 
 module.exports = (chain, seeds) => {
-    words = [];
+  const words = [];
 
-    seed = seeds[Math.floor(Math.random() * seeds.length)];
+  let seed = seeds[Math.floor(Math.random() * seeds.length)];
 
-    while(seed !== '||') {
-        words.push(seed);
-        seed = feedforward(chain.get(seed));
-    }
+  while (seed !== '||') {
+    words.push(seed);
+    seed = feedforward(chain.get(seed));
+  }
 
-    return words.join(' ');
-}
+  return words.join(' ');
+};
